f54f6ab6cc427b6366fd7886fe8dac5b818fdfb0
semaiwald <107176297+semaiwald@users.noreply.github.com>
2022-06-24 09:47:17 +0200

    change vm name

--- ci-test-vm/main.tf
+++ ci-test-vm/main.tf
@@ -5,18 +5,18 @@ provider "hcloud" {
 
 # Create a new SSH key
 resource "hcloud_ssh_key" "ansible-test-ssh-key" {
-  name = "Ansible Test VM SSH Key"
+  name = "SeMa_ansitest01 SSH Key"
   public_key = file(var.ssh_key)
 }
 
 # Create a server
 resource "hcloud_server" "ansible-test-vm" {
-  name = "ansible-test-instance"
+  name = "SeMa_ansitest01"
   image = "ubuntu-20.04"
   server_type = "cx11"
   location = "nbg1"
   ssh_keys = [
-    "Ansible Test VM SSH Key"
+    "SeMa_ansitest01 SSH Key"
   ]
 
   provisioner "local-exec" {
