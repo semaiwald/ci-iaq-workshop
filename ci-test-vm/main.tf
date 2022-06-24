0a725ab3bbccbbf6643328baf45a51cd40110a83
semaiwald <107176297+semaiwald@users.noreply.github.com>
2022-06-24 10:06:34 +0200

    fix typo

--- ci-test-vm/main.tf
+++ ci-test-vm/main.tf
@@ -5,18 +5,18 @@ provider "hcloud" {
 
 # Create a new SSH key
 resource "hcloud_ssh_key" "ansible-test-ssh-key" {
-  name = "SeMa_ansitest01 SSH Key"
+  name = "SeMa-ansitest01 SSH Key"
   public_key = file(var.ssh_key)
 }
 
 # Create a server
 resource "hcloud_server" "ansible-test-vm" {
-  name = "SeMa_ansitest01"
+  name = "SeMa-ansitest01"
   image = "ubuntu-20.04"
   server_type = "cx11"
   location = "nbg1"
   ssh_keys = [
-    "SeMa_ansitest01 SSH Key"
+    "SeMa-ansitest01 SSH Key"
   ]
 
   provisioner "local-exec" {
